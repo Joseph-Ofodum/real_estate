9bdd24d8c1c8944708b85b4b7cb520cdd80e9554
Joseph Ofodum <josephofodum@example.com>
2024-06-27 15:07:34 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AgentController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
+
+// Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])
+// ->name('admin.dashboard');
+
+// Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])
+// ->name('agent.dashboard');
+
+// assigning the middleware to the various class of roles.
+
+Route::middleware(['auth', 'roleMiddleware:admin'])->group(function () {
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+});
+
+// Apply the roleMiddleware to the agent dashboard route
+Route::middleware(['auth', 'roleMiddleware:agent'])->group(function () {
+    Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
+});
+
